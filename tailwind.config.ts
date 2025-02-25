d9908165dede729ff617d3a96995e09423ed7560
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-02-25 11:54:48 +0000

    initalize project

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          from: "hsl(236, 72%, 79%)",
+          to: "hsl(237, 63%, 64%)",
+        },
+        neutral: {
+          "very-light-grayish-blue": "hsl(240, 78%, 98%)",
+          "light-grayish-blue": "hsl(234, 14%, 74%)",
+          "grayish-blue": "hsl(233, 13%, 49%)",
+          "dark-grayish-blue": "hsl(232, 13%, 33%)",
+        },
+      },
+      fontFamily: {
+        montser: ["var(--font-geist-montserrat)", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
